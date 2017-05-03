eb04f3629184b8d9c7113499df1f8346bc8973cf
Mikael Mello <mikaelmello@hotmail.com>
2017-05-02 21:40:56 -0300

    Versao melhorada e menor do codigo em Assembly

--- Bandeiras/flag.s
+++ Bandeiras/flag.s
@@ -1,18 +1,19 @@
 .data
 
-BR: .asciiz "brasil.bin"
+BRASIL: .asciiz "brasil.bin"
 USA: .asciiz "usa.bin"
-JP: .asciiz "japao.bin"
-BT: .asciiz "butao.bin"
+JAPAO: .asciiz "japao.bin"
+BUTAO: .asciiz "butao.bin"
+LAMAR: .asciiz "lamar.bin"
 
 .text
 
-	jal BRASIL
+	la $a0, LAMAR
+	jal FLAG
 
 	j END
 
-BUTAO:
-	la $a0,BT
+FLAG:
 	li $a1,0
 	li $a2,0
 	li $v0,13
@@ -35,69 +36,5 @@ BUTAO:
 	syscall
 	
 	jr $ra
-JAPAO:
-	la $a0,JP
-	li $a1,0
-	li $a2,0
-	li $v0,13
-	syscall
-	
-	# Transfer all the file to the VGA memory
-	move $a0,$v0
-	la $a1,0xFF000000
-	li $a2,76800
-	li $v0,14
-	syscall
-
-	# Closes the file
-	li $v0,16
-	syscall
-	li $v0,10
-	syscall
-	
-	jr $ra
-EUA:
-	la $a0,USA
-	li $a1,0
-	li $a2,0
-	li $v0,13
-	syscall
-	
-	# Transfer all the file to the VGA memory
-	move $a0,$v0
-	la $a1,0xFF000000
-	li $a2,76800
-	li $v0,14
-	syscall
-
-	# Closes the file
-	li $v0,16
-	syscall
-	li $v0,10
-	syscall
-	
-	jr $ra
-BRASIL:
-	la $a0,BR
-	li $a1,0
-	li $a2,0
-	li $v0,13
-	syscall
-	
-	# Transfer all the file to the VGA memory
-	move $a0,$v0
-	la $a1,0xFF000000
-	li $a2,76800
-	li $v0,14
-	syscall
-
-	# Closes the file
-	li $v0,16
-	syscall
-	li $v0,10
-	syscall
-	
-	jr $ra
-	
 	
 END:
