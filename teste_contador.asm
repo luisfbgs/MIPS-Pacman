715e90a10705d62f21b59df81a6bbaf229f3201f
leooleo <leo.ms097@gmail.com>
2017-06-01 18:49:26 -0300

    TESTE COM CONTADOR

---
+++ teste_contador.asm
@@ -0,0 +1,526 @@
+.data
+	out1: 	.asciiz "Error! "
+	out2: 	.asciiz "Acerto: "
+	out3:	.asciiz "All 35 tests succeeded!"
+	in1: 	.word 1
+	in2: 	.word 2
+	in3: 	.float 1.5
+	in4: 	.float 4.0
+	in5: 	.float 2.0
+	in6: 	.float -2.0
+	cpmuls: .float 6.0
+	cpadds: .float 5.5
+	cpsubs: .float 2.5
+	estate: .word  0
+.text
+li $s7,0
+li $s6,0
+
+j main
+
+acertor:
+	addi $s6,$s6,1
+	jr $ra
+error: 
+	addi $sp,$sp,-4
+	sw $a0,0($sp)
+
+	#addi $v0,$s7,104
+	addi $v0,$s7,4
+	la $a0,out1
+	lw $a1,estate
+	li $a3,0xFF00
+	syscall
+	
+	lw $a0,0($sp)
+	addi $sp,$sp,4
+	li $a2,0
+	li $a3,0xFF00
+	#li $v0,101
+	li $v0,1
+	syscall #print int
+	
+	addi $a1,$a1,3
+	sw $a1,estate
+	
+	
+	li $v1,-1
+	jr $ra
+	
+soma: 	
+	lw $t0,in1
+	lw $t1,in2
+	li $t2,3
+	li $a0,1
+	
+	add $t0,$t0,$t1
+	bne $t0,$t2,error
+	j acertor
+	jr $ra
+		
+multiplicacao:
+
+
+
+	lw $t0,in1
+	lw $t1,in2
+	li $t2,2
+	li $a0,2
+	
+	mult $t0,$t1
+	mflo $t0
+	
+	bne $t0,$t2,error
+	j acertor
+	jr $ra
+	
+subtracao:	
+
+	lw $t0,in1
+	lw $t1,in2
+	li $t2,-1
+	li $a0,3
+	
+	sub $t0,$t0,$t1
+	bne $t0,$t2,error
+	j acertor
+	jr $ra
+	
+op_and:
+	lw $t0,in1
+	lw $t1,in2
+	li $t2,0
+	li $a0,4
+	
+	and $t0,$t0,$t1
+	
+	bne $t0,$t2,error
+	j acertor
+	jr $ra	
+	
+op_or:	
+	
+	lw $t0,in1
+	lw $t1,in2
+	li $t2,3
+	li $a0,5
+	
+	or $t0,$t0,$t1
+	
+	bne $t0,$t2,error
+	j acertor
+	jr $ra	
+	
+mfhi_mflo:	
+	addi $sp,$sp,-4
+	sw $ra,0($sp)
+
+	lw $t0,in1
+	lw $t1,in2
+	li $t2,2
+	li $a0,6
+	
+	mult $t0,$t1
+	
+	mflo $t0	
+	bne $t0,$t2,mfhi_mflo_chama_error
+	j try	
+	
+	mfhi_mflo_chama_error: j error
+	try:
+	jal acertor
+	lw $ra,0($sp)
+	addi $sp,$sp,4
+	
+	move $t2,$zero
+	li $a0,7
+	mfhi $t0
+	bne $t0,$t2,error
+	j acertor
+	jr $ra
+	
+op_sll:
+	lw $t0,in1
+	lw $t1,in2
+	li $t2,4
+	li $a0,8
+	
+	sll $t0,$t0,2
+	
+	bne $t0,$t2,error
+	j acertor
+	jr $ra	
+	
+op_srl:
+	lw $t0,in2
+	li $t2,1
+	li $a0,9
+	
+	srl $t0,$t0,1
+	
+	bne $t0,$t2,error
+	j acertor
+	jr $ra	
+	
+op_slt:
+	lw $t0,in1
+	li $t1,-1
+	li $t2,0
+	li $a0,10
+	
+	slt $t0,$t0,$t1
+	
+	bne $t0,$t2,error
+	j acertor
+	jr $ra
+op_sgt:
+	lw $t0,in1
+	lw $t1,in2
+	li $t2,1
+	li $a0,11
+	
+	sgt $t0,$t1,$t0
+	
+	bne $t0,$t2,error
+	j acertor
+	jr $ra
+	
+op_sra:
+	li $t0,32
+	li $t2,8
+	li $a0,12
+	
+	sra $t0,$t0,2
+	
+	bne $t0,$t2,error
+	j acertor
+	jr $ra
+	
+op_srav:
+	li $t0,32
+	li $t1,2
+	li $t2,8
+	li $a0,13
+	
+	srav $t0,$t0,$t1
+	
+	bne $t0,$t2,error
+	j acertor
+	jr $ra	
+	
+op_sllv:
+	li $t0,2
+	li $t1,2
+	li $t2,8
+	li $a0,14
+	
+	sllv $t0,$t0,$t1
+	
+	bne $t0,$t2,error
+	j acertor
+	jr $ra	
+	
+op_srlv:
+	li $t0,32
+	li $t1,1
+	li $t2,16
+	li $a0,15
+	
+	srlv $t0,$t0,$t1
+	
+	bne $t0,$t2,error
+	j acertor
+	jr $ra
+	
+op_xor:
+	li $t0,2
+	li $t1,3
+	li $t2,1
+	li $a0,16
+	
+	xor $t0,$t0,$t1
+	
+	bne $t0,$t2,error
+	j acertor
+	jr $ra	
+	
+op_nor:
+	li $t0,0
+	li $t1,0
+	li $t2,-1
+	li $a0,17
+	
+	nor $t0,$t0,$t1
+	
+	bne $t0,$t2,error
+	j acertor
+	jr $ra	
+	
+op_sltu:
+	lw $t0,in1
+	li $t1,-1
+	li $t2,1
+	li $a0,18
+	
+	sltu $t0,$t0,$t1
+	
+	bne $t0,$t2,error
+	j acertor
+	jr $ra			
+	
+op_lui:
+	lui $t0,0xFFFF
+	la $t2,0xFFFF0000
+	li $a0,19	
+	
+	bne $t0,$t2,error
+	j acertor
+	jr $ra	
+	
+op_div:
+	addi $sp,$sp,-4
+	sw $ra,0($sp)
+
+	li $t0,31
+	li $t1,2
+	li $t2,15
+	li $a0,20
+		
+	div $t0,$t1
+	mflo $t0
+	
+	bne $t0,$t2,error	
+	jal acertor
+	
+	mfhi $t0
+	li $t2,1
+	li $a0,21
+	bne $t0,$t2,error
+	
+	lw $ra,0($sp)
+	addi $sp,$sp,4
+	j acertor
+	jr $ra
+	
+op_mthi_mtlo:
+	addi $sp,$sp,-4
+	sw $ra,0($sp)
+	li $t0,2
+	li $a0,22
+	li $t2,2
+	
+	mthi $t0
+	mfhi $t1
+	
+	bne $t1,$t2,chamaerror
+	jal acertor
+	j try2
+	chamaerror: j error
+	
+	try2:lw $ra,0($sp)	
+	addi $sp,$sp,4
+		
+	li $a0,23
+	mtlo $t0
+	mflo $t1
+	
+	bne $t1,$t2,error
+	j acertor
+	jr $ra	
+	
+op_muls: 
+	l.s $f0,in3																																																					
+	l.s $f1,in4	
+	l.s $f2,cpmuls		#f2 = 6
+	li $a0,24
+	
+	mul.s $f0,$f0,$f1
+	
+	c.eq.s $f0,$f2
+	bc1f error
+	j acertor
+	
+	jr $ra		
+	
+op_adds:
+	l.s $f0,in3																																																					
+	l.s $f1,in4	
+	l.s $f2,cpadds	
+	li $a0,25
+	
+	add.s $f0,$f0,$f1
+	
+	c.eq.s $f0,$f2
+	bc1f error
+	j acertor
+	
+	jr $ra			
+
+op_subs:
+	l.s $f0,in3
+	l.s $f1,in4
+	l.s $f2,cpsubs
+	li $a0,26
+	
+	sub.s $f0,$f1,$f0
+	
+	c.eq.s $f0,$f2
+	bc1f error
+	j acertor
+	
+	jr $ra
+
+op_divs:
+	l.s $f0,in5
+	l.s $f1,in4
+	li $a0,27
+	
+	div.s $f1,$f1,$f0
+	
+	c.eq.s $f0,$f1
+	bc1f error
+	j acertor
+	
+	jr $ra
+
+op_sqrt:
+	l.s $f0,in5
+	l.s $f1,in4
+	li $a0,28
+	
+	sqrt.s $f1,$f1
+	
+	c.eq.s $f1,$f0
+	bc1f error
+	j acertor
+	
+	jr $ra
+
+op_abs:
+	l.s $f0,in5
+	l.s $f1,in6
+	li $a0,29
+	
+	abs.s $f1,$f1
+	
+	c.eq.s $f1,$f0
+	bc1f error
+	j acertor
+	jr $ra
+	
+op_neg:
+	l.s $f0,in5
+	l.s $f1,in6
+	li $a0,30
+	
+	neg.s $f0,$f0
+	
+	c.eq.s $f1,$f0
+	bc1f error
+	j acertor
+	jr $ra
+	
+op_ceq:
+	l.s $f0,in5
+	l.s $f1,in5
+	li $a0,31
+
+	c.eq.s $f1,$f0
+	bc1f error
+	j acertor
+	jr $ra
+	
+op_clt:
+	l.s $f0,in4
+	l.s $f1,in5
+	li $a0,32
+
+	c.lt.s $f1,$f0
+	bc1f error
+	j acertor
+	jr $ra
+
+op_cle:
+	l.s $f0,in4
+	l.s $f1,in5
+	li $a0,33
+
+	c.le.s $f1,$f0
+	bc1f error
+	j acertor
+	jr $ra
+	
+op_cvtsw:
+	l.s $f0,in5
+	lw $t0,in2
+	cvt.w.s $f1,$f0
+	mfc1 $t1,$f1
+	li $a0,34
+
+	beq $t1,$t0,sai
+	j error 
+sai:
+	j acertor
+	jr $ra
+
+op_cvtws:
+	lw $t0,in2
+	mtc1 $t0,$f1
+	cvt.s.w $f1,$f1
+	l.s $f0,in5
+	li $a0,35
+
+	c.eq.s $f1,$f0
+	bc1f error
+	j acertor
+	jr $ra
+	
+testa_acertos:
+	li $t0,35
+	bne $t0,$s6,pula_testa_acertos
+	la $a0,out3
+	li $a1,60
+	li $a2,90
+	li $a3,0xFF00
+	li $v0,104
+	syscall
+	pula_testa_acertos: jr $ra
+		
+main: 
+	#ULA			#codigo de erro
+	jal soma 		#1
+	jal multiplicacao 	#2
+	jal subtracao 		#3
+	jal op_and		#4
+	jal op_or		#5
+	jal mfhi_mflo		#6/7
+	jal op_sll		#8
+	jal op_srl		#9
+	jal op_slt		#10
+	jal op_sgt		#11
+	jal op_sra		#12
+	jal op_srav		#13
+	jal op_sllv		#14
+	jal op_srlv		#15
+	jal op_xor		#16
+	jal op_nor		#17	
+	jal op_sltu		#18
+	jal op_lui		#19
+	jal op_div		#20/21
+	jal op_mthi_mtlo	#22/23
+	
+	#FPULA
+	jal op_muls		#24
+	jal op_adds		#25
+	jal op_subs		#26
+	jal op_divs		#27
+	jal op_sqrt		#28
+	jal op_abs		#29
+	jal op_neg		#30
+	jal op_ceq		#31
+	jal op_clt		#32
+	jal op_cle		#33
+	jal op_cvtsw		#34
+	jal op_cvtws		#35
+	
+	jal testa_acertos			
